e9d3db3c837c000b71f17ba9a9cbc00fdb66ac6b
Hakan Yalniz <hakan_yalniz@live.com>
2023-10-14 16:44:51 +0300

    Changed the style of search bar.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,8 +4,12 @@ export default function SearchBar() {
   return (
     <div className="search-container">
       <form action="#">
-        <input type="text" placeholder="Search.." name="search" />
-        <button type="submit">Submit</button>
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search.."
+          name="search"
+        />
       </form>
     </div>
   );
