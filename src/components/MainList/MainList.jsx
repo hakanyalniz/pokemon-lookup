ea20a9e690f5d3bd13dd909011a68529a3fd4d85
Hakan Yalniz <hakan_yalniz@live.com>
2023-10-14 16:59:38 +0300

    Created folders and did some outlining for MainList, which will be a list of pokemon.

---
+++ src/components/MainList/MainList.jsx
@@ -0,0 +1,6 @@
+// Use dynamic routing
+// In this approach, you create a single template for the Pokémon detail page.
+// The specific data for each Pokémon is then loaded dynamically based on the URL parameter.
+
+// For example, if you're using a JavaScript framework like React, you might have a route like `/pokemon/:id`,
+// where `:id` is a placeholder for the Pokémon ID. When a user navigates to `/pokemon/1`, your app would fetch the data for Pokémon with ID 1 and display it using the detail page template.
