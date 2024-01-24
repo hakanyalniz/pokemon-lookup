06bf51cdf17fe0bf8f0a4f99b0b12f68d91ee1ca
Hakan Yalniz <hakan_yalniz@live.com>
2024-01-24 16:33:12 +0300

    Added data for the Training and Breeding tables in pokemon detail page.

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -264,8 +264,13 @@ export default function PokemonDetail() {
                     <th>Abilities</th>
                     <td>
                       {currentPokemon[4].abilities.map((abilities, index) => (
-                        <a href={abilities.ability.url} key={index}>
-                          {abilities.ability.name + "\n"}{" "}
+                        <a
+                          href={abilities.ability.url}
+                          target="_blank"
+                          rel="noreferrer"
+                          key={index}
+                        >
+                          {abilities.ability.name + "\n"}
                         </a>
                       ))}
                     </td>
@@ -274,8 +279,57 @@ export default function PokemonDetail() {
               </table>
             </div>
             <div className="additionalInfo">
-              <span className="sub-title">Training</span>
-              <span className="sub-title">Breeding</span>
+              <div className="training-data">
+                <span className="sub-title">Training</span>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Capture Rate</th>
+                      <td>{currentPokemon[9].capture_rate}</td>
+                    </tr>
+                    <tr>
+                      <th>Base Friendship</th>
+                      <td>{currentPokemon[8].base_happiness}</td>
+                    </tr>
+                    <tr>
+                      <th>Base Exp</th>
+                      <td>{currentPokemon[7].base_experience}</td>
+                    </tr>
+                    <tr>
+                      <th>Growth Rate</th>
+                      <td>{currentPokemon[11].growth_rate.name}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="breeding-data">
+                <span className="sub-title">Breeding</span>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Egg Groups</th>
+                      <td>
+                        {currentPokemon[10].egg_groups.map(
+                          (egg_item, index) => (
+                            <a
+                              href={egg_item.url}
+                              target="_blank"
+                              rel="noreferrer"
+                              key={index}
+                            >
+                              {egg_item.name + "\n"}
+                            </a>
+                          )
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Egg Cycles</th>
+                      <td>{currentPokemon[13].hatch_counter}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </>
