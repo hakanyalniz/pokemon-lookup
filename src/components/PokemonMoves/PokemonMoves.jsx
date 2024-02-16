4fe829a0c6a28cd899431b63faa4756c9130e0cc
Hakan Yalniz <hakan_yalniz@live.com>
2024-02-16 17:31:02 +0300

    Added some style to pretty up the generation picker, alongside a text which shows the generation.

--- src/components/PokemonMoves/PokemonMoves.jsx
+++ src/components/PokemonMoves/PokemonMoves.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import "./PokemonMoves.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PokemonMoves({ currentPokemon }) {
   const [currentGeneration, setCurrentGeneration] = useState(1);
@@ -63,46 +63,75 @@ export default function PokemonMoves({ currentPokemon }) {
 
   const handleSelectGeneration = (event) => {
     event.stopPropagation(); // Prevent event propagation
+    // It returns nothing if anything other than a number has been pressed
+    if (isNaN(Number(event.target.textContent))) return;
 
-    console.log(event.target.textContent);
+    // Makes everything normal, after that only sets the clicked target bold
+    // They all need to be set normal, or all clicked will become bold
+    const generationPicker = document.getElementById("generation-picker");
+    if (generationPicker) {
+      const childElements = generationPicker.querySelectorAll("*");
+      childElements.forEach((element) => {
+        element.style.fontWeight = "normal";
+      });
+    }
+    event.target.style.fontWeight = "bold";
+
+    // console.log(Number(event.target.textContent));
+
+    setCurrentGeneration(Number(event.target.textContent));
   };
-
-  // const generationPicker = document.getElementById("generation-picker");
-  // if (generationPicker) {
-  //   console.log("running");
-  //   generationPicker.addEventListener("click", (event) => {
-  //     selectGeneration(event);
-  //   });
-  // }
 
   // console.log(versionGroupMoves);
 
-  return (
-    <>
-      <span className="sub-title">Pokemon Moves</span>
-      <div id="generation-picker" onClick={handleSelectGeneration}>
-        Other generations <span>1</span> | <span>2</span> | <span>3</span> |{" "}
-        <span>4</span> | <span>5</span> | <span>6</span> | <span>7</span> |{" "}
-        <span>8</span> | <span>9</span>
-      </div>
+  const generationPickerText = () => {
+    const switchArray = {
+      1: "Red/Blue and Yellow",
+      2: "Gold/Silver and Crystal",
+      3: "Ruby/Sapphire, FireRed/LeafGreen and Emerald",
+      4: "Diamond/Pearl, Platinum and HeartGold/SoulSilver",
+      5: "Black/White and Black 2/White 2",
+      6: "X/Y and Omage Ruby/Alpha Sapphire",
+      7: "Sun/Moon, Ultra Sun/ Ultra Moon, Let's Go Pikachu/Let's Go Eevee",
+      8: "Sword/Shield, Brilliant Diamond/Shining Pearl and Legends: Arceus",
+      9: "Scarlet/Violet",
+    };
+    return switchArray[currentGeneration];
+  };
 
-      <div>
-        <span className="sub-sub-title">Moves learnt by level up</span>
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th></th>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+  useEffect(() => {
+    console.log(currentGeneration);
+  }, [currentGeneration]);
+
+  if (currentGeneration)
+    return (
+      <>
+        {/* Perhaps change the number spans to buttons and make them look ordinary again with CSS */}
+        <span className="sub-title">Pokemon Moves</span>
+        <div id="generation-picker" onClick={handleSelectGeneration}>
+          Other generations <span>1</span> | <span>2</span> | <span>3</span> |{" "}
+          <span>4</span> | <span>5</span> | <span>6</span> | <span>7</span> |{" "}
+          <span>8</span> | <span>9</span>
+          <div style={{ fontSize: "18px" }}>{generationPickerText()}</div>
+        </div>
+
+        <div>
+          <span className="sub-sub-title">Moves learnt by level up</span>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th></th>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
 }
